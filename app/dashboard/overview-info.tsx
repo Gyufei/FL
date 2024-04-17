35664d0c90680082f2f64a4f15ab8fd3307acb52
fei guo <812773673@qq.com>
2024-04-17 17:24:12 +0800

    fix:date-picker-confirm-logic

--- app/dashboard/overview-info.tsx
+++ app/dashboard/overview-info.tsx
@@ -1,15 +1,13 @@
 "use client";
-import { formatNum } from "@/lib/utils/number";
 import Image from "next/image";
+import { formatNum } from "@/lib/utils/number";
 import { Input } from "../../components/ui/input";
 import { useAccountOverview } from "@/lib/hooks/api/use-account-overview";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import { Calendar } from "@/components/ui/calendar";
 import { DateRange } from "react-day-picker";
 import { useState } from "react";
 import { format, subDays } from "date-fns";
-import { Button } from "@/components/ui/button";
 import { TadleXp } from "./tadle-xp";
+import DateRangePickerDialog from "@/components/share/date-range-picker-dialog";
 
 export default function OverviewInfo() {
   const [date, setDate] = useState<DateRange | undefined>({
@@ -33,55 +31,28 @@ export default function OverviewInfo() {
             </div>
           </div>
 
-          <Dialog>
-            <DialogTrigger asChild>
-              <div className="flex h-12 items-center space-x-2 rounded-full bg-[#fafafa] px-5 text-sm leading-5 text-black">
-                <Image
-                  src="/icons/calendar.svg"
-                  width={24}
-                  height={24}
-                  alt="calendar"
-                />
-                {date?.from ? (
-                  date.to ? (
-                    <div className="flex flex-col">
-                      <span>{format(date.from, "LLL dd, y")}</span>
-                      <span>{format(date.to, "LLL dd, y")}</span>
-                    </div>
-                  ) : (
-                    <div>{format(date.from, "LLL dd, y")}</div>
-                  )
+          <DateRangePickerDialog dateRange={date} setDateRange={setDate}>
+            <div className="flex h-12 items-center space-x-2 rounded-full bg-[#fafafa] px-5 text-sm leading-5 text-black">
+              <Image
+                src="/icons/calendar.svg"
+                width={24}
+                height={24}
+                alt="calendar"
+              />
+              {date?.from ? (
+                date.to ? (
+                  <div className="flex flex-col">
+                    <span>{format(date.from, "LLL dd, y")}</span>
+                    <span>{format(date.to, "LLL dd, y")}</span>
+                  </div>
                 ) : (
-                  <span>Pick a date</span>
-                )}
-              </div>
-            </DialogTrigger>
-            <DialogContent
-              className="w-[838px] border-none bg-white p-4"
-              showClose={false}
-            >
-              <div>
-                <Calendar
-                  initialFocus
-                  mode="range"
-                  defaultMonth={date?.from}
-                  selected={date}
-                  onSelect={setDate}
-                  numberOfMonths={2}
-                  formatters={{
-                    formatCaption: (month: Date) => {
-                      return format(month, "yyyy - MM");
-                    },
-                  }}
-                />
-                <div className="mt-5 flex justify-end border-t border-[#F0F1F5] pt-4">
-                  <Button className="h-7 rounded-full border border-[#eee]">
-                    Confirm
-                  </Button>
-                </div>
-              </div>
-            </DialogContent>
-          </Dialog>
+                  <div>{format(date.from, "LLL dd, y")}</div>
+                )
+              ) : (
+                <span>Pick a date</span>
+              )}
+            </div>
+          </DateRangePickerDialog>
         </div>
 
         {/* overview */}
---
+++ components/share/date-range-picker-dialog.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import { DateRange } from "react-day-picker";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Calendar } from "@/components/ui/calendar";
+import { format, subDays } from "date-fns";
+import { Button } from "@/components/ui/button";
+
+export default function DateRangePickerDialog({
+  dateRange,
+  setDateRange,
+  children,
+}: {
+  dateRange: DateRange | undefined;
+  setDateRange: (date: DateRange | undefined) => void;
+  children: React.ReactNode;
+}) {
+  const [range, setRange] = useState<DateRange | undefined>({
+    from: subDays(new Date(), 5),
+    to: undefined,
+  });
+
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (dateRange) {
+      setRange(dateRange);
+    }
+  }, [dateRange]);
+
+  function handleConfirm() {
+    setDateRange(range);
+    setOpen(false);
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent
+        className="w-[838px] border-none bg-white p-4"
+        showClose={false}
+      >
+        <div>
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={range?.from}
+            selected={range}
+            onSelect={setRange}
+            numberOfMonths={2}
+            formatters={{
+              formatCaption: (month: Date) => {
+                return format(month, "yyyy - MM");
+              },
+            }}
+          />
+          <div className="mt-5 flex justify-end border-t border-[#F0F1F5] pt-4">
+            <Button
+              onClick={handleConfirm}
+              className="h-7 rounded-full border border-[#eee]"
+            >
+              Confirm
+            </Button>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}

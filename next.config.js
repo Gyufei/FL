226954b5ee8a6552bf23f688ef7f78db241cba40
fei guo <812773673@qq.com>
2024-05-31 14:48:07 +0800

    fix:add-prod-cdn

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "cdn.tadle.com",
+      },
+      {
+        protocol: "https",
         hostname: "explorer-api.walletconnect.com",
       },
       {

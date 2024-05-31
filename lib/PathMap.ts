297587ddd308b7db9d519a43af306a198e46e5ca
fei guo <812773673@qq.com>
2024-05-31 14:47:36 +0800

    fix:preview-env

--- lib/PathMap.ts
+++ lib/PathMap.ts
@@ -1,34 +1,33 @@
+const isProduction = process.env.NODE_ENV === 'production' && !process.env.NEXT_PUBLIC_IS_PREVIEW
+
 export function WithHost(path: string) {
   const devHost = `https://preview-api.tadle.com`
   const prodHost = `https://api.tadle.com`
-  const host = process.env.NODE_ENV === 'production' ? prodHost : devHost
+  const host = isProduction ? prodHost : devHost
   return `${host}${path}`
 }
 
 function WithCDN(path: string) {
   const devCDN = `https://preview-cdn.tadle.com`
   const prodCDN = `https://cdn.tadle.com`
-  const cdn = process.env.NODE_ENV === 'production' ? prodCDN : devCDN
+  const cdn = isProduction ? prodCDN : devCDN
   return `${cdn}/images${path}`;
-  // return `${process.env.NEXT_PUBLIC_CDN_URL}${path}`;
 }
 
 export function WithProjectCDN(path: string) {
   const goPath = path.endsWith(".png") ? path : `${path}.png`;
   return WithCDN(`/project/${goPath}`)
-  // return `${process.env.NEXT_PUBLIC_CDN_URL}${path}`;
 }
 
 export function WithPointCDN(path: string) {
   const goPath = path.endsWith(".png") ? path : `${path}.png`;
   return WithCDN(`/point/${goPath}`)
-  // return `${process.env.NEXT_PUBLIC_CDN_URL}${path}`;
 }
 
 export function WithWss() {
   const devWss = "preview-wss.tadle.com";
   const prodWss = "wss://wss.tadle.com";
-  const wss = process.env.NODE_ENV === 'production' ? prodWss : devWss
+  const wss = isProduction ? prodWss : devWss
   return wss;
 }
 

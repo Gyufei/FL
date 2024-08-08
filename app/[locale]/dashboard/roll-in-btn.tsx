bf1951ccb86d0e6b4d70ae653609cb27ead3fab3
fei guo <812773673@qq.com>
2024-08-08 18:16:25 +0800

    fix:error-click-rollin

--- app/[locale]/dashboard/roll-in-btn.tsx
+++ app/[locale]/dashboard/roll-in-btn.tsx
@@ -1,3 +1,4 @@
+import WithWalletConnectBtn from "@/components/share/with-wallet-connect-btn";
 import { useRollin } from "@/lib/hooks/contract/use-rollin";
 import { useWalletConnect } from "@/lib/hooks/web3/use-wallet-connect";
 import { isSameDay } from "date-fns";
@@ -18,7 +19,8 @@ export default function RollInBtn() {
   const [isSign, setIsSign] = useState(false);
 
   function handleSign() {
-    if (isLoading) return;
+    if (isLoading || isSign) return;
+
     rollinAction(undefined);
   }
 
@@ -43,12 +45,13 @@ export default function RollInBtn() {
   }, [isSuccess]);
 
   return (
-    <div
-      onClick={handleSign}
-      data-sign={isSign}
-      className="flex h-7 w-[74px] cursor-pointer items-center justify-center rounded-[52px] border border-[#FFA95B] text-sm leading-5 text-[#FFA95B] data-[sign=true]:cursor-default data-[sign=true]:border-[#d3d4d6] data-[sign=true]:text-[#d3d4d6]"
-    >
-      {T("btn-Rollin")}
-    </div>
+    <WithWalletConnectBtn shouldSignIn={true} onClick={handleSign}>
+      <div
+        data-sign={isSign}
+        className="flex h-7 w-[74px] cursor-pointer items-center justify-center rounded-[52px] border border-[#FFA95B] text-sm leading-5 text-[#FFA95B] data-[sign=true]:cursor-default data-[sign=true]:border-[#d3d4d6] data-[sign=true]:text-[#d3d4d6]"
+      >
+        {T("btn-Rollin")}
+      </div>
+    </WithWalletConnectBtn>
   );
 }

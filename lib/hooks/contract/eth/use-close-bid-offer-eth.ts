670fabd79a2f2fea32b257fd1033fe8684632041
fei guo <812773673@qq.com>
2024-08-23 17:10:50 +0800

    fix:eth-temp-hide

--- lib/hooks/contract/eth/use-close-bid-offer-eth.ts
+++ lib/hooks/contract/eth/use-close-bid-offer-eth.ts
@@ -1,34 +1,23 @@
-import { PreMarketABI } from "@/lib/abi/eth/pre-markets";
+// import { PreMarketABI } from "@/lib/abi/eth/pre-markets";
 import { useEthConfig } from "../../web3/use-eth-config";
 import { useWriteContract } from "wagmi";
 import { useCallback } from "react";
 
-export function useCloseBidOfferEth({
-  marketplaceStr,
-  makerStr,
-  offerStr,
-  isNativeToken,
-}: {
-  marketplaceStr: string;
-  makerStr: string;
-  offerStr: string;
-  isNativeToken: boolean;
-}) {
+export function useCloseBidOfferEth() {
   const { ethConfig } = useEthConfig();
 
   const { data, error, isError, isPending, isSuccess, writeContract } =
     useWriteContract();
 
   const txAction = useCallback(() => {
-    const abiAddress = ethConfig.contracts.preMarket;
-
-    return writeContract({
-      abi: PreMarketABI,
-      address: abiAddress as any,
-      functionName: "closeBidOffer",
-      args: [],
-    });
-  }, [writeContract, ethConfig, marketplaceStr, offerType]);
+    // const abiAddress = ethConfig.contracts.preMarket;
+    // return writeContract({
+    //   abi: PreMarketABI,
+    //   address: abiAddress as any,
+    //   functionName: "closeBidOffer",
+    //   args: [],
+    // });
+  }, [writeContract, ethConfig]);
 
   return {
     data,

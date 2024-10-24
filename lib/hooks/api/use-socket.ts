31c8adbeddaa3d5d88d90dc6b0329a3d2e6e7fc5
fei guo <812773673@qq.com>
2024-10-24 15:43:13 +0800

    fix:ws-socket-error

--- lib/hooks/api/use-socket.ts
+++ lib/hooks/api/use-socket.ts
@@ -1,9 +1,12 @@
 import { io } from "socket.io-client";
 import { useEndPoint } from "./use-endpoint";
 
+let SocketInst: any;
+
 export function useSocket() {
   const { wssEndPoint } = useEndPoint();
-  const socket = io(wssEndPoint);
+  const socket = SocketInst || io(wssEndPoint);
+  SocketInst = socket;
 
   return {
     socket,

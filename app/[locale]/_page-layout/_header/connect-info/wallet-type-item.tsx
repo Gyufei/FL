19961fa323a0850d18eaa17273b70c97cc995744
Derek Li <derekli2024@gmail.com>
2024-10-31 11:38:53 +0800

    feat: add privy status

---
+++ app/[locale]/_page-layout/_header/connect-info/wallet-type-item.tsx
@@ -0,0 +1,179 @@
+import React, { useState } from "react";
+import { ChevronDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import { usePrivy } from "@privy-io/react-auth";
+// import { useSetActiveWallet } from "@privy-io/wagmi";
+import { truncateAddr } from "@/lib/utils/web3";
+import { useChainWallet } from "@/lib/hooks/web3/use-chain-wallet";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+import { ChainConfigs } from "@/lib/const/chain-config";
+
+interface AddressEntry {
+  address: string;
+  linked: boolean;
+  [key: string]: any;
+}
+
+export interface WalletType {
+  name: string;
+  icon: string;
+  addresses: AddressEntry[];
+  types: string[];
+  chainList: string[];
+  selectedChain: string;
+}
+export default function WalletTypeItem({
+  walletType,
+  updateSelectedChain,
+}: {
+  walletType: WalletType;
+  updateSelectedChain: (walletType: string, chain: string) => void;
+}) {
+  // const { setActiveWallet } = useSetActiveWallet();
+  const { disconnect } = useChainWallet();
+
+  const [popOpen, setPopOpen] = useState(false);
+
+  const { linkWallet } = usePrivy();
+
+  const handleChainSelect = (chain: string) => {
+    updateSelectedChain(walletType.name, chain);
+    setPopOpen(false); // 选择后关闭下拉菜单
+  };
+
+  const handleAddAddress = () => {
+    linkWallet();
+  };
+
+  const handleConnect = (link: any) => {
+    if (link.linked) {
+      if (["okx_wallet", "metamask"].includes(link.walletClientType)) {
+        disconnect();
+      }
+      link.disconnect();
+    } else {
+      link.loginOrLink();
+      // setActiveWallet(link);
+    }
+  };
+
+  return (
+    <>
+      <div key={walletType.name} className="space-y-3">
+        <div className="flex items-center space-x-2">
+          <Image
+            src={
+              walletType.selectedChain
+                ? ChainConfigs[walletType.selectedChain]?.logo
+                : walletType.icon
+            }
+            alt={walletType.name}
+            width={24}
+            height={24}
+            className="rounded-full"
+          />
+          <span className="text-base font-medium">
+            {walletType.selectedChain
+              ? `${walletType.name}（ ${
+                  ChainConfigs[walletType.selectedChain]?.name
+                } ）`
+              : walletType.name}
+          </span>
+          {walletType.selectedChain && (
+            <Popover
+              open={popOpen}
+              onOpenChange={(isOpen) => setPopOpen(isOpen)}
+            >
+              <PopoverTrigger asChild>
+                <div
+                  data-state={popOpen ? "open" : "close"}
+                  className="cursor-pointer data-[state=open]:rotate-180"
+                >
+                  <ChevronDown className="text-gray-500 h-4 w-4" />
+                </div>
+              </PopoverTrigger>
+              <PopoverContent
+                className="z-[200] flex w-[218px] flex-col items-stretch space-y-2 border-none bg-white p-2"
+                style={{
+                  boxShadow: "0px 0px 10px 0px rgba(45, 46, 51, 0.1)",
+                }}
+                align="start"
+              >
+                {walletType.chainList.map((chain) => (
+                  <div
+                    key={chain}
+                    onClick={() => handleChainSelect(chain)}
+                    data-state={
+                      chain === walletType.selectedChain ? "active" : "inactive"
+                    }
+                    className="flex cursor-pointer items-center justify-start space-x-3 rounded-xl px-4 py-3 text-black data-[state=active]:bg-[#FAFAFA]"
+                  >
+                    <Image
+                      width={24}
+                      height={24}
+                      src={ChainConfigs[chain]?.logo}
+                      alt="evms"
+                      className="z-10 rounded-full bg-white"
+                    ></Image>
+                    <div className="flex-1 text-xs">
+                      {ChainConfigs[chain]?.name}
+                    </div>
+                  </div>
+                ))}
+              </PopoverContent>
+            </Popover>
+          )}
+        </div>
+        {walletType.addresses.map((entry, addressIndex) => (
+          <div key={addressIndex} className="flex items-center space-x-3">
+            <div className="flex-grow">
+              <div className="relative">
+                <Input
+                  value={truncateAddr(entry.address || "", {
+                    nPrefix: 12,
+                    nSuffix: 9,
+                  })}
+                  readOnly
+                  className="w-full rounded-xl border-[#FAFAFA] bg-[#FAFAFA] pr-10 focus:border-[#3DD866] focus:bg-white"
+                />
+                {entry.address && (
+                  <Image
+                    onClick={() => handleConnect(entry)}
+                    src={
+                      entry.linked
+                        ? "/icons/disconnect.svg"
+                        : "/icons/rpc-link.svg"
+                    }
+                    width={24}
+                    height={24}
+                    alt="link"
+                    className="absolute right-3 top-2 cursor-pointer"
+                  />
+                )}
+              </div>
+            </div>
+            <Button
+              variant="outline"
+              size="icon"
+              className="bounded-[12px] h-11 w-11 border-[#D8D8D8]"
+              onClick={() => handleAddAddress()}
+            >
+              <Image
+                src="/icons/ac-plus.svg"
+                width={24}
+                height={24}
+                alt="add"
+              />
+            </Button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
---
+++ app/[locale]/_page-layout/_header/connect-info/wallets.tsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from "react";
+import { useWallets, useSolanaWallets, usePrivy } from "@privy-io/react-auth";
+import { useChainWallet } from "@/lib/hooks/web3/use-chain-wallet";
+import { useTranslations } from "next-intl";
+import WalletTypeItem from "./wallet-type-item";
+import { useAccount } from "wagmi";
+export default function Component() {
+  const t = useTranslations("Header");
+
+  const [walletTypes, setWalletTypes] = useState<any[]>([]);
+
+  const { wallets } = useWallets();
+  const { wallets: solanaWallets } = useSolanaWallets();
+  const { logout } = usePrivy();
+  const { disconnect } = useChainWallet();
+
+  const { address } = useAccount();
+
+  useEffect(() => {
+    if (wallets.length > 0 || solanaWallets.length > 0) {
+      const chainWallets = wallets.filter((w: any) =>
+        walletTypes[0]?.selectedChain === "bnb"
+          ? w.type !== "ethereum"
+          : w.type === "ethereum",
+      );
+      const newWalletTypes = [
+        {
+          name: "Evm",
+          icon: "/icons/op.svg",
+          chainList: ["eth", "bnb"],
+          selectedChain: walletTypes[0]?.selectedChain || "eth",
+          addresses:
+            chainWallets.length < 1
+              ? [{}]
+              : chainWallets.map((wallet) => ({
+                  ...wallet,
+                  linked: address === wallet.address,
+                })),
+          types: ["ethereum"],
+        },
+        {
+          name: "Solana",
+          icon: "/icons/solana-black.svg",
+          addresses:
+            solanaWallets.length < 1
+              ? [{}]
+              : solanaWallets.map((wallet) => ({
+                  ...wallet,
+                  linked: address === wallet.address,
+                  walletClientType: wallet.walletClientType,
+                })),
+          types: ["solana"],
+        },
+      ];
+      if (JSON.stringify(walletTypes) !== JSON.stringify(newWalletTypes)) {
+        setWalletTypes(newWalletTypes);
+      }
+    }
+  }, [wallets, solanaWallets, walletTypes, address]);
+
+  const handleChainSelect = (walletType: string, chain: string) => {
+    console.log(
+      "🚀 ~ handleChainSelect ~ walletType, chain:",
+      walletType,
+      chain,
+    );
+    const newWalletTypes = walletTypes.map((wallet) => {
+      if (wallet.name === walletType) {
+        return {
+          ...wallet,
+          selectedChain: chain,
+        };
+      }
+      return wallet;
+    });
+    setWalletTypes(newWalletTypes);
+  };
+  const handleLogout = () => {
+    wallets.forEach((wallet) => wallet.disconnect());
+    disconnect();
+    logout();
+  };
+
+  return (
+    <>
+      <div className="font-video mx-auto w-full max-w-md space-y-4 p-4 text-[14px] ">
+        {walletTypes.map((walletType, walletIndex) => (
+          <WalletTypeItem
+            key={walletIndex}
+            walletType={walletType}
+            updateSelectedChain={handleChainSelect}
+          />
+        ))}
+      </div>
+      <div className="mt-10 w-full">
+        <button
+          onClick={handleLogout}
+          className="flex h-12 w-full items-center justify-center rounded-2xl border border-red bg-white text-red hover:bg-red hover:text-white"
+        >
+          {t("btn-SignOut")}
+        </button>
+      </div>
+    </>
+  );
+}

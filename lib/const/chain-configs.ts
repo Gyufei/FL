faa47dd70702a4bc9df0ab7056a56a1155b17ed2
Derek Li <derekli2024@gmail.com>
2024-11-05 18:26:34 +0800

    fix: update bnb test tokenManager

--- lib/const/chain-configs.ts
+++ lib/const/chain-configs.ts
@@ -94,7 +94,7 @@ export const ChainConfigs: Record<string, IChainConfig> = {
         }
       : {
           preMarkets: "0xa853BE4931401059Dce7146b28aC4A190f108354",
-          tokenManager: "0xa921e0BA08ceA8850D82D5e8240f626777FC1dB9",
+          tokenManager: "0x8146e3d854BdC8BdC7c054DCAeC94B7AFa6438A3",
           systemConfig: "0xa026b4E35AAE30f7CC5F0a205D49b8A38d1B65Aa",
           deliveryPlace: "0x384124A2588a8a446873a34c0FdFfE7f30FfE70F",
           mdin: "0x734D5aB96eEAFE1F8BA36186627FAd08E7fF7026",

bab9dc40c07754be1f1bc48026f90560d39dd150
Derek Li <derekli2024@gmail.com>
2024-11-12 15:21:54 +0800

    fix: bsc rpc

--- lib/const/chain-configs.ts
+++ lib/const/chain-configs.ts
@@ -61,7 +61,6 @@ export const ChainConfigs: Record<string, IChainConfig> = {
           tokenManager: "0xa921e0BA08ceA8850D82D5e8240f626777FC1dB9",
           systemConfig: "0xa026b4E35AAE30f7CC5F0a205D49b8A38d1B65Aa",
           deliveryPlace: "0x384124A2588a8a446873a34c0FdFfE7f30FfE70F",
-          mdin: "0x734D5aB96eEAFE1F8BA36186627FAd08E7fF7026",
         }
       : {
           // test
@@ -69,7 +68,6 @@ export const ChainConfigs: Record<string, IChainConfig> = {
           tokenManager: "0x87f25fe11280c82aEf7247157A06525Cff7A13e5",
           systemConfig: "0xf29140CEE701A202215CC59800Ddd9a4382eD20f",
           deliveryPlace: "0x30681E123b2eC25157f52f3d52baB1EBD8fb5450",
-          mdin: "0x734D5aB96eEAFE1F8BA36186627FAd08E7fF7026",
         },
     isEvm: true,
   },
@@ -81,7 +79,7 @@ export const ChainConfigs: Record<string, IChainConfig> = {
     network: isProduction ? 56 : 97,
     rpcs: {
       TadleDefaultRPC: isProduction
-        ? process.env.NEXT_PUBLIC_DEFAULT_RPC_BSC || "https://rpc.ankr.com/bnb"
+        ? process.env.NEXT_PUBLIC_DEFAULT_RPC_BSC || "https://rpc.ankr.com/bsc"
         : "https://rpc.ankr.com/bsc_testnet_chapel",
     },
     contracts: isProduction
@@ -90,14 +88,12 @@ export const ChainConfigs: Record<string, IChainConfig> = {
           tokenManager: "0xa921e0BA08ceA8850D82D5e8240f626777FC1dB9",
           systemConfig: "0xa026b4E35AAE30f7CC5F0a205D49b8A38d1B65Aa",
           deliveryPlace: "0x384124A2588a8a446873a34c0FdFfE7f30FfE70F",
-          mdin: "0xD5d45f6Cf9E357818e25862c5EAb77495b27C87c",
         }
       : {
           preMarkets: "0xa853BE4931401059Dce7146b28aC4A190f108354",
           tokenManager: "0x8146e3d854BdC8BdC7c054DCAeC94B7AFa6438A3",
           systemConfig: "0xa026b4E35AAE30f7CC5F0a205D49b8A38d1B65Aa",
           deliveryPlace: "0x384124A2588a8a446873a34c0FdFfE7f30FfE70F",
-          mdin: "0xD5d45f6Cf9E357818e25862c5EAb77495b27C87c",
         },
     isEvm: true,
   },

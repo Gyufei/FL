2fc4b8aee4ed3bc95e8f4c17add93efd3c73b3e9
Derek Li <derekli2024@gmail.com>
2024-11-13 21:21:09 +0800

    feat: add referral table mobil ui

--- app/[locale]/dashboard/referral/page.tsx
+++ app/[locale]/dashboard/referral/page.tsx
@@ -36,7 +36,7 @@ export default function Referral() {
   }
 
   return (
-    <div className="flex h-full flex-1 flex-col px-4 sm:ml-5 sm:px-0">
+    <div className="flex h-full w-screen flex-1 flex-col px-4 sm:ml-5 sm:w-full sm:px-0">
       <div className="hidden items-center justify-between sm:flex">
         <div className="flex items-center space-x-5">
           <div className="text-xl leading-[30px] text-black">
@@ -47,20 +47,28 @@ export default function Referral() {
 
       <div className="relative mt-5 flex flex-1 flex-col justify-start border-t border-[#eee]">
         <ReferralInfo referralData={referralData || []} />
-        <div className="mb-2 mt-5 flex items-center space-x-2">
-          <div className="text-base leading-6 text-black">
-            {rt("cap-ReferralLink")}
+        <div className="mt-4 rounded-xl bg-white px-4 sm:mt-0 sm:bg-transparent">
+          <div className="mb-2 mt-5 flex items-center space-x-2">
+            <div className="text-base leading-6 text-black">
+              {rt("cap-ReferralLink")}
+            </div>
+            <Image
+              onClick={handleCreate}
+              width={20}
+              height={20}
+              src="/icons/circle-add.svg"
+              alt="add"
+              className="cursor-pointer"
+            />
           </div>
-          <Image
-            onClick={handleCreate}
-            width={20}
-            height={20}
-            src="/icons/circle-add.svg"
-            alt="add"
-            className="cursor-pointer"
-          />
+          <div className=" overflow-x-scroll sm:max-w-none sm:overflow-x-hidden sm:px-0">
+            <div className="max-h-auto relative min-h-[296px] w-[820px] overflow-y-hidden sm:w-full sm:min-w-0">
+              <div className="absolute bottom-0 left-0 right-0 top-0">
+                <ReferralTable referralData={referralData} refresh={refetch} />
+              </div>
+            </div>
+          </div>
         </div>
-        <ReferralTable referralData={referralData} refresh={refetch} />
       </div>
     </div>
   );

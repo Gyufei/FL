8cf87e347c3aa629e3ef1992f7b13969f1da069e
Derek Li <derekli2024@gmail.com>
2024-11-29 19:25:34 +0800

    feat: open test vconsole2

--- app/[locale]/marketplace/[...name]/page.tsx
+++ app/[locale]/marketplace/[...name]/page.tsx
@@ -6,7 +6,7 @@ import { useWsMsgSub } from "@/lib/hooks/api/use-ws-msgs";
 import { useEffect } from "react";
 import { ChainType } from "@/lib/types/chain";
 import VConsole from "vconsole";
-import { isProduction } from "@/lib/PathMap";
+// import { isProduction } from "@/lib/PathMap";
 // if (!isProduction) {
 new VConsole();
 // }

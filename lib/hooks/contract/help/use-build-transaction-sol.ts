8c52d77bafe6d1f4c23180b65372ae3110539c82
fei guo <812773673@qq.com>
2024-12-13 19:45:48 +0800

    fix:solana-call

--- lib/hooks/contract/help/use-build-transaction-sol.ts
+++ lib/hooks/contract/help/use-build-transaction-sol.ts
@@ -4,27 +4,26 @@ import {
   VersionedTransaction,
   Keypair,
 } from "@solana/web3.js";
+
 export function useBuildTransactionSol() {
-  const { publicKey, wallet, sendTransaction } = useWallet();
+  const { sendTransaction } = useWallet();
   const { connection } = useConnection();
 
   const buildTransaction = async (callParams: any) => {
-    const versionedMessage = VersionedMessage.deserialize(
-      Uint8Array.from(Buffer.from(callParams.data, "hex")),
-    );
-    const recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
-    versionedMessage.recentBlockhash = recentBlockhash;
-
-    const versionedTransaction = new VersionedTransaction(versionedMessage);
-    // versionedTransaction.feePayer = publicKey;
-
     const seed_account = Keypair.fromSecretKey(
       new Uint8Array(callParams.accounts[0]),
     );
-    versionedTransaction.sign([seed_account]);
+    const recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
 
-    // await connection.simulateTransaction(versionedTransaction);
-    const txHash = await sendTransaction(versionedTransaction, connection);
+    const versionedMessage = VersionedMessage.deserialize(
+      Uint8Array.from(Buffer.from(callParams.data, "hex")),
+    );
+    versionedMessage.recentBlockhash = recentBlockhash;
+    const versionedTransaction = new VersionedTransaction(versionedMessage);
+
+    const txHash = await sendTransaction(versionedTransaction, connection, {
+      signers: [seed_account],
+    });
 
     return txHash;
   };

3698b191051331c4d8b2736939fbce0acff3dbd8
Derek Li <derekli2024@gmail.com>
2025-03-13 23:25:02 +0800

    feat: open test market

--- lib/hooks/api/use-marketplaces.ts
+++ lib/hooks/api/use-marketplaces.ts
@@ -27,7 +27,7 @@ export function useMarketplaces(chain?: string) {
           projectLogo: WithProjectImgCDN(m.market_symbol, chain),
           pointLogo: WithPointImgCDN(m.market_symbol, chain),
           chain,
-          status: m.market_symbol !== "hadron" ? "offline" : m.status,
+          status: m.market_symbol === "spherex" ? "offline" : m.status,
         };
       });
 
